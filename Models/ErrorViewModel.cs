0ae6788aac79561e85cef09cf95854cc099fa340
Jonathan Reynosa <jonnyjjv@hotmail.com>
2020-08-04 09:57:29 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace googledriveclone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
